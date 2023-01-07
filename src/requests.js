af1da39c0bf0894cd9b58aa9bf995425a66166e6
BinaryKiwiz <alan.kw.shi@gmail.com>
2023-01-07 00:58:10 -0800

    Implement search

--- src/requests.js
+++ src/requests.js
@@ -1,3 +1,5 @@
+import Trail from "./trail.js";
+
 const apiLink = "https://hackathonbackend-bnldi3x7oq-uw.a.run.app/api/";
 const Get = (id) => {return document.getElementById(id)};
 
@@ -22,8 +24,26 @@ export async function getReq(path, link = apiLink){
         }
     })
     .catch((err) => console.log(err))
-    .then(response => response.text())
-    .then(response => console.log(response));
+    .then((res) => res.json())
+    .then((data) => {
+        for(let i = 0; i < data.length; i++){
+            const dict = data[i];
+            const trail = new Trail(dict.name, dict.zipcode, dict.city, dict.state, dict.distance, dict.longitude, dict.latitude, dict.condition);
+            Get("trail-list").appendChild(trail.getHTML);
+        }
+    })
+}
+
+export async function checkServer(link = apiLink){
+    fetch(link + "checkServer/", {
+        method: "GET",
+        headers:{
+            "Content-Type" : "application/json"
+        }
+    })
+    .catch((err) => console.log(err))
+    .then((res) => res.json())
+    .then((res) => console.log(res))
 }
 
 export async function patchReq(data, path, query, link = apiLink){
