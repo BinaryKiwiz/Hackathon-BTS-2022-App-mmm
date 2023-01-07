e58fa4802a95511cd897d7b61940231d29f92b14
BinaryKiwiz <alan.kw.shi@gmail.com>
2023-01-06 22:04:19 -0800

    Start implementing search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import Trail from "./trail.js";
 import {postReq, getReq} from "./requests.js";
 
+const trailAPI = "https://prescriptiontrails.org/api/";
 const Get = (id) => {return document.getElementById(id)}; //shorthand for fetching from DOM
 
 const testTrail = new Trail("Test Trail", 91362, "Thousand Oaks", "CA", 100, 50, "horrible");
 Get("trail-list").appendChild(testTrail.getHTML);
 
-const searchTrails = () => {
-
+const searchTrails = (queryType, query) => {
+    if(queryType == "city"){
+        byCity(query);
+    }
 }
+
+const byCity = (city) => {
+    fetch(trailAPI + "filter/?by=city&city=" + city + "&offset=0&count=10", {
+        method: "GET"
+    })
+    .catch((err) => console.log(err))
+    .then((response) => response.json())
+    .then((response) => JSON.parse(response))
+    .then((data) => {
+        console.log(data);
+    })
+}
+
+console.log(document.querySelector("input[name = 'search-by']:checked"));
+
+//Get("trail-search-query").value
 
 getReq("checkServer/");
 
-Get("search-trails").addEventListener(searchTrails());
+Get("search-trails").addEventListener("click", () => {
+    const queryType = document.querySelector("input[name = 'search-by']:checked");
+    const query = Get("trail-search-query").value;
+
+    if(!queryType || !query){
+        alert("Please input valid search options");
+        return;
+    }
+
+    searchTrails(queryType.id, query);
+});
