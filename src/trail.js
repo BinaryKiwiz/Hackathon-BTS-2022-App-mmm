1a36e53b83a634bc4cc10e9750e91af0b3bd5641
BinaryKiwiz <alan.kw.shi@gmail.com>
2023-01-07 07:16:21 -0800

    Add user names and fix listings

--- src/trail.js
+++ src/trail.js
@@ -1,6 +1,6 @@
 import { postReq } from "./requests.js";
 
-const colors = {"GOOD" : "green", "MID" : "yellow", "BAD" : "orange", "HORRIBLE" : "red"};
+const colors = {"GOOD" : "#55cf55", "MID" : "#fff15c", "BAD" : "#ffbb5c", "HORRIBLE" : "#fa6048"};
 
 class Trail{
     constructor(name, zipcode, city, state, dist, longitude, latitude, condition){
@@ -24,14 +24,32 @@ class Trail{
         const id = urlParams.get("id");
 
         let elm = document.createElement("li");
+        elm.style.display = "flex";
+        elm.style.flexDirection = "row";
+        elm.style.alignItems = "center";
+        elm.style.gap = "1em";
+        elm.classList.add("listed-trail");
         let trailElm = document.createElement("div");
         
         let title = document.createElement("a");
+        title.style.padding = "1em";
         title.href = `trail.html?trailid=${this.id}&id=${id}`;
         title.appendChild(document.createTextNode(this.name));
         trailElm.appendChild(title);
 
+        let cond = document.createElement("div");
+        cond.appendChild(document.createTextNode("Condition: " + this.condition));
+
+        let zip = document.createElement("div");
+        zip.appendChild(document.createTextNode("Zipcode: " + this.zipcode));
+
+        let distance = document.createElement("div");
+        distance.appendChild(document.createTextNode("Distance: " + this.dist));
+
         elm.appendChild(trailElm);
+        elm.appendChild(cond);
+        elm.appendChild(distance);
+        elm.appendChild(zip);
         elm.style.backgroundColor = colors[this.condition];
         
         return elm;
